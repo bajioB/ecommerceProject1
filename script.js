1376182b83379d42161756cc7f0de115350bc0a7
moti <belinamoti@email.com>
2022-05-26 00:23:31 -0400

    5/26/22

--- script.js
+++ script.js
@@ -41,3 +41,69 @@ for (let i = 0; i < smallimg.length; i++) {
     
     }
 }
+
+// CART/SHOP PAGE
+
+
+var removeCartItemButtons = document.getElementsByClassName('cart-remove');
+console.log(removeCartItemButtons);
+
+for (var i=0; i<removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i];
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target;
+        buttonClicked.parentElement.parentElement.parentElement.remove();
+        updateCartTotal();
+
+    })
+
+    var quantityInput = document.getElementsByClassName('cart-quantity-input');
+    for (var i = 0; i < quantityInput.length; i++) {
+        var input = quantityInput[i];
+        input.addEventListener('change', quantityChanged);
+    }
+
+    // var addToCartButtons = document.getElementsByClassName('add-to-cart');
+    // for (var i = 0; i < addToCartButtons.length; i++) {
+    //     var button = addToCartButtons[i];
+    //     button.addEventListener('click', addToCartClicked);
+    // }
+}
+
+// function addToCartClicked(event) {
+//     var button = event.target; 
+//     var shopItem = button.parentElement.parentElement;
+//     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+//     var price = shopItem.getAttributeNames('price')[0].innerText;
+//     console.log(price, title);
+     
+// }
+
+function quantityChanged(event) {
+    var input = event.target;
+    if(isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updateCartTotal();
+}
+
+function updateCartTotal () {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    var total = 0;
+    for (var i=0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var totalPrice = cartRow.getElementsByClassName('cart-total')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+
+        var price = parseFloat(priceElement.innerText.replace('$', ''));
+        var quantity = quantityElement.value;
+        totalPrice.innerText = '$' + (price * quantity);
+        total = total + (price * quantity);
+    
+    }
+    
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total;
+}
+
